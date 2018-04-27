27e08af4b0533664db1be2289084d8c635d8e6ca
hebiao6446 <15921994335@163.com>
2018-04-27 14:54:52 +0800

    传输完成，服务端完成

--- main.cpp
+++ main.cpp
@@ -47,9 +47,47 @@ void diep(char *s)
 
 
 
+char ** split_buffer(char *pYuvBuf,unsigned long max_length,unsigned long * arr_size){
+
+
+    unsigned long total =  strlen(pYuvBuf);
+
+    unsigned long count = total/max_length +(total%max_length==0?0:1);
+
+    *arr_size = count;
+
+    unsigned long tail_length = total - (count-1)*max_length;
+
+    char **c_list =(char **)calloc(count, sizeof(char *));
+
+    for (int kk = 0; kk<count; kk++) {
+        if (kk == count-1) {
+            c_list[kk] = (char *)calloc(tail_length,sizeof(char));
+        }else{
+            c_list[kk] = (char *)calloc(max_length,sizeof(char));
+        }
+
+    }
+
+    for (int i=0; i<count; i++) {
+        if (i == count -1) {
+            memcpy(c_list[i],pYuvBuf +i*max_length, tail_length);
+        }else{
+            memcpy(c_list[i],pYuvBuf +i*max_length, max_length);
+        }
+    }
+
+    return c_list;
+}
+
+
+
+
+
+
 int main() {
 
-    /*
+
 
     CvCapture *capture = cvCreateCameraCapture(0);
     IplImage *frame;
@@ -61,6 +99,8 @@ int main() {
     int frameW    = (int) cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
 
 
+    int yuv_bufLen = frameW*frameH*3/2;
+    unsigned char* pYuvBuf = new unsigned char[yuv_bufLen];
 
     int bufsize;
     x264Encoder x264;
@@ -70,7 +110,7 @@ int main() {
 
 
 
-*/
+
 
 
 
@@ -92,6 +132,9 @@ int main() {
 
 
 
+
+
+
     client c_;
 
     while (1){
@@ -101,7 +144,7 @@ int main() {
             printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
             c_.host = si_other.sin_addr.s_addr;
             c_.port = si_other.sin_port;
-            
+
 
             break;
         }
@@ -111,10 +154,97 @@ int main() {
 
 
 
-    while (1){
-//        frame = cvQueryFrame(capture);
-//        Mat mat_img=cvarrToMat(frame);
 
+
+
+
+
+
+
+
+
+
+    char *temp="a";
+    char *msg;
+    int  kk = 0;
+    int max_str_len = 9216;
+    msg=(char *)malloc(max_str_len*strlen(temp)+1);
+    strcpy(msg,temp);
+    for(kk=1;kk<max_str_len;kk++)
+        strcat(msg,temp);
+    size_t lenth =  strlen(msg);
+
+
+
+
+//    int  send_max_data_length =  9216;
+
+    int  send_max_data_length =  1000;
+    while (1){
+        frame = cvQueryFrame(capture);
+        Mat mat_img=cvarrToMat(frame);
+
+
+        Mat yuvImg;
+
+        cvtColor(mat_img, yuvImg, CV_BGR2YUV_I420);
+        memcpy(pYuvBuf, yuvImg.data, yuv_bufLen*sizeof(unsigned char));
+
+
+
+        si_other.sin_addr.s_addr = c_.host;
+        si_other.sin_port = c_.port;
+
+        printf("Sending to %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+
+        printf("============== %d",yuv_bufLen);
+//        1382400
+
+        unsigned long arr_size ;
+        char  ** arr_buffer = split_buffer((char *)pYuvBuf,send_max_data_length,&arr_size);
+
+
+        char  *start = "start";
+        if (sendto(s, start, strlen(start), 0, (struct sockaddr*)(&si_other), slen)==-1)
+            diep("sendto");
+        for (int k = 0; k < arr_size; ++k) {
+            char *m_chr = arr_buffer[k];
+            if (sendto(s, m_chr, strlen(m_chr), 0, (struct sockaddr*)(&si_other), slen)==-1)
+                diep("sendto");
+        }
+
+        char  *end = "end";
+        if (sendto(s, end, strlen(end), 0, (struct sockaddr*)(&si_other), slen)==-1)
+            diep("sendto");
+
+        /*
+        int count = (yuv_bufLen/send_max_data_length) + (yuv_bufLen%send_max_data_length==0?0:1);
+
+        char *copy_header=(char *)malloc(send_max_data_length+1);
+
+
+        int tail_length = yuv_bufLen-(count-1)*send_max_data_length;
+
+        char *copy_tail =  (char *)malloc(tail_length+1);
+
+        for (int k = 0; k < count; ++k) {
+
+            if (k == count - 1){
+                memcpy(copy_tail,pYuvBuf +k*send_max_data_length, tail_length);
+                if (sendto(s, copy_tail, tail_length, 0, (struct sockaddr*)(&si_other), slen)==-1)
+                    diep("sendto");
+            } else{
+                memcpy(copy_header ,pYuvBuf+k*send_max_data_length, send_max_data_length);
+                if (sendto(s, copy_header, send_max_data_length, 0, (struct sockaddr*)(&si_other), slen)==-1)
+                diep("sendto");
+            }
+
+        }
+         */
+
+
+//        if (sendto(s, pYuvBuf, yuv_bufLen, 0, (struct sockaddr*)(&si_other), slen)==-1)
+//            diep("sendto");
 
 
         /*
@@ -168,18 +298,19 @@ int main() {
 
 
 
-
+        /*
 
             si_other.sin_addr.s_addr = c_.host;
             si_other.sin_port = c_.port;
 
             printf("Sending to %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
 
-           char  *msg = "这是一段代码测试，看看字符串中文行不行";
-           size_t lenth =  strlen(msg);
-           if (sendto(s, msg, lenth, 0, (struct sockaddr*)(&si_other), slen)==-1)
+            if (sendto(s, msg, lenth, 0, (struct sockaddr*)(&si_other), slen)==-1)
                     diep("sendto");
 
+        */
+
+//        7968 4006701855
 
 
 
